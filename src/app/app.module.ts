3a869dc79ed2aa2f8539fedec56a68d834393361
Robbe Mahieu <merendipity99@gmail.com>
2023-07-29 18:20:56 +0200

    Updated folder structure and added gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Build command: ng build --output-path docs --base-href /RobbeMahieu.github.io/
+
 @NgModule({
   declarations: [
     AppComponent
