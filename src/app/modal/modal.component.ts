91ea647dbf305966b7fb07c72885f3b08cb8982f
Robbe Mahieu <merendipity99@gmail.com>
2023-09-10 11:26:01 +0200

    added auto next image in galleries

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,18 +1,25 @@
-import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnDestroy, Output } from '@angular/core';
 import { Project } from '../models/project';
+import { Observable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements OnDestroy {
   @Input() project: Project = new Project("ello", "", "", [], []);
   public galleryIndex: number = 0;
 
+  private autoIncrementObservable: Subscription = interval(5000).subscribe(() => this.ChangeIndex(1, true));
+
   @Output() clickedOutside: EventEmitter<void> = new EventEmitter<void>();
 
-  public ChangeIndex(amount: number) {
+  ngOnDestroy() {
+    this.autoIncrementObservable.unsubscribe();
+  }
+
+  public ChangeIndex(amount: number, isAutoUpdate: boolean = false) {
     this.galleryIndex += amount;
 
     while (this.galleryIndex < 0) {
@@ -20,6 +27,12 @@ export class ModalComponent {
     }
 
     this.galleryIndex %= this.project.images.length;
+
+    // Restart timer if not automatic
+    if (!isAutoUpdate) {
+      this.autoIncrementObservable.unsubscribe();
+      this.autoIncrementObservable = interval(5000).subscribe(() => this.ChangeIndex(1, true));
+    }
   }
 
   @HostListener("click", ['$event']) OnClick(event: MouseEvent) {
